a129587fc0eb5cc21fccf9e7448baa5056b4d07b
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-05-13 11:08:51 +0300

    debugged config.py

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 class Config:
-
-
-
-  class ProdConfig(Config):
     pass
 
 
+class ProdConfig(Config):
+    pass
+
 class DevConfig(Config):
     DEBUG = True
-
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
